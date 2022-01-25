a2db68e47e5c65a9634f5e3393bf344fd3f56a95
KrisGum <krisgumularz@gmail.com>
2022-01-25 20:26:46 +0100

    Fix subpagetitle test

--- src/components/SubpageTitle/SubpageTitle.test.js
+++ src/components/SubpageTitle/SubpageTitle.test.js
@@ -1,5 +1,5 @@
+import { render } from "@testing-library/react";
 import SubpageTitle from "./SubpageTitle";
-import { render, screem } from "@testing-library/react";
 
 describe("SubpageTitle", () => {
   test("Check the subpageTitle was render", () => {
