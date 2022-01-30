15fce2d1dc193dca28fc092161020927fb4506dc
Marcin <marcinkukulka@gmail.com>
2022-01-30 18:45:39 +0100

    set react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./container/Navbar/Navbar";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <nav>
+        <Routes>
+          {/* <Route path="/home" element={<HomePage />} />
+          <Route path="/browse" element={<BrowsePage />} />
+          <Route path="/add-recipe" element={<AddRecipe />} />
+          <Route path="/generate-menu" element={<GenerateMenu />} /> */}
+          <Route/>
+        </Routes>
+      </nav>
+    </>
+  );
 };
 
 export default App;
