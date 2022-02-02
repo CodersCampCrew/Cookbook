78141e797a98cbc4bc10fc1ca1c60941aee9862f
kostnerek <kostekmc@gmail.com>
2022-02-02 12:38:41 +0100

    created home page structure with premade components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Navbar from "./container/Navbar/Navbar";
+import "./app.scss";
+import HomePage from "./container/HomePage/HomePage";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+    </>
+  );
 };
 
 export default App;
--- src/components/HomePage/HomePageRecommendedLabel.js
+++ src/components/HomePage/HomePageRecommendedLabel.js
@@ -3,20 +3,20 @@ import propTypes from "prop-types";
 import classes from "./HomePage.module.scss";
 
 const RecommendedLabel = ({ text }) => {
-    return (
-      <div className={classes.dishNameStyle}>
-        <p className={classes.recommendedStyle}>RECOMMENDED</p>
-        <br />
-        {text}
-      </div>
-    );
-   };
-   
-   RecommendedLabel.propTypes = {
-    text: propTypes.string
-   };
-   RecommendedLabel.defaultProps = {
-    text: "Dish"
-   };
-   
-   export default RecommendedLabel;
+  return (
+    <div className={classes.dishNameStyle}>
+      <div className={classes.recommendedStyle}>RECOMMENDED</div>
+      <br />
+      <div className={classes.dishName}>{text}</div>
+    </div>
+  );
+};
+
+RecommendedLabel.propTypes = {
+  text: propTypes.string
+};
+RecommendedLabel.defaultProps = {
+  text: "Dish"
+};
+
+export default RecommendedLabel;
