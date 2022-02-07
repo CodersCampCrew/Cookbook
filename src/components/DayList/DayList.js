5873846b9d8d23917fc966a3b53a94e5c1ac7e47
Przemysław Gajowczyk <przemyslaw.gajowczyk23@gmail.com>
2022-02-07 12:51:54 +0100

    initial fix

--- src/components/DayList/DayList.js
+++ src/components/DayList/DayList.js
@@ -1,15 +1,24 @@
 import React from "react";
+import propTypes from "prop-types";
 import classes from "./DayList.module.scss";
 
-const DayList = () => {
+const DayList = ({onChange}) => {
   return (
     <div className={classes.dayTitle}>
-      <select className={classes.selectday}>
-        <option value="Day1">Day 1</option>
-        <option value="Day2">Day 2</option>
+      <select onChange={onChange} className={classes.selectday}>
+        <option value="1">Day 1</option>
+        <option value="2">Day 2</option>
       </select>
     </div>
   );
 };
 
 export default DayList;
+
+DayList.propTypes = {
+  onChange: propTypes.func
+};
+
+DayList.defaultProps = {
+  onChange: () => {},
+};
--- src/pages/GeneratedMenuPage/GeneratedMenuPage.js
+++ src/pages/GeneratedMenuPage/GeneratedMenuPage.js
@@ -1,46 +1,90 @@
+import React, { useState } from "react";
 import MealSnapShot from "../../components/MealSnapShot/MealSnapShot";
 import DayList from "../../components/DayList/DayList";
 import classes from "./GeneratedMenuPage.module.scss";
 import SubpageTitle from "../../components/SubpageTitle/SubpageTitle";
 
-const DUMMY_DISHES = [
-  {
-    id: "d1",
-    img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
-    title: "scrambled eggs",
-    kcal: "500",
-    time: 5,
-    desc: "simple, tasty, day starter, to wake you up",
-    dishType: "Breakfast"
-  },
-  {
-    id: "d2",
-    img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
-    title: "schabowy",
-    kcal: "500",
-    time: 5,
-    desc: "great Polish dish",
-    dishType: "Lunch"
-  },
-  {
-    id: "d3",
-    img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
-    title: "Ramen",
-    kcal: "500",
-    time: 5,
-    desc: "great Japanese dish",
-    dishType: "Dinner"
+
+ const days = [
+  { day: 1,
+    dishes: [
+      {
+        id: "d1",
+        img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
+        title: "scrambled eggs",
+        kcal: "500",
+        time: 5,
+        desc: "simple, tasty, day starter, to wake you up",
+        dishType: "Breakfast"
+      },
+      {
+        id: "d2",
+        img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
+        title: "schabowy",
+        kcal: "500",
+        time: 5,
+        desc: "great Polish dish",
+        dishType: "Lunch"
+      },
+      {
+        id: "d3",
+        img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
+        title: "Ramen",
+        kcal: "500",
+        time: 5,
+        desc: "great Japanese dish",
+        dishType: "Dinner"
+      }
+    ] },
+  { day: 2,
+    dishes: [
+        {
+          id: "d1",
+          img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
+          title: "scrambled eggs",
+          kcal: "500",
+          time: 5,
+          desc: "simple, tasty, day starter, to wake you up",
+          dishType: "Breakfast"
+        },
+        {
+          id: "d2",
+          img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
+          title: "schabowy",
+          kcal: "500",
+          time: 5,
+          desc: "great Polish dish",
+          dishType: "Lunch"
+        },
+        {
+          id: "d3",
+          img: "https://i.picsum.photos/id/884/100/100.jpg?hmac=HMwxDNALxMBZgAa1RBpR_sK2iwXb4d7PpowbCgRIrGM",
+          title: "Ramen",
+          kcal: "500",
+          time: 5,
+          desc: "great Japanese dish",
+          dishType: "Dinner"
+        }
+      ]
   }
-];
+ ]
+ 
+
+
 
 const GeneratedMenuPage = () => {
+  const [day, setDay] = useState("1");
+  const handleChange =(e)=>{
+    setDay(e.target.value);
+    console.log(day);
+  }
   return (
     <>
       <div className={classes.centered}>
-        <DayList />
+        <DayList onChange={handleChange}/>
       </div>
       <ul>
-        {DUMMY_DISHES.map((dish) => (
+        {days.map((dish) => (
           <>
             <SubpageTitle
               className={classes.title}
