948e78a175556445da837575e43d5d9eeb76cc95
kostnerek <kostekmc@gmail.com>
2022-02-08 21:17:00 +0100

    fixed navbar

--- src/container/Navbar/Navbar.js
+++ src/container/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, React } from "react";
 import TextInput from "../../components/TextInput/TextInput";
 import classes from "./Navbar.module.scss";
 import SlideoutMenu from "../../components/SlideoutMenu/SlideoutMenu";
@@ -10,31 +10,55 @@ const Navbar = () => {
     jsx-a11y/click-events-have-key-events, 
     jsx-a11y/no-noninteractive-element-interactions 
   */
+  const [visibility, setVisibility] = useState(false);
+
+  const getVisibilityClass = () => {
+    if (visibility) {
+      return classes.visible;
+    }
+    return classes.hidden;
+  };
+
   const search = (param) => {
     //  search function placeholder to be implemented
     console.log(param);
   };
   return (
     <div className={classes.navbar} id="outer-container">
-      <div className={classes["logo-container"]}>
-        <img
-          className={classes.logo}
-          src="https://picsum.photos/100/100"
-          alt=""
-        />
-        <div className={classes.name}>The&nbsp;CookBook</div>
+      <div className={classes.logoContainer}>
+        <div className={classes.name}>
+          <img
+            className={classes.logo}
+            src="https://picsum.photos/100/100"
+            alt=""
+          />
+          The&nbsp;CookBook
+        </div>
       </div>
-      <div className={classes["search-container"]}>
-        <TextInput placeholder="Search" callback={search} />
-        <img
-          className={classes["search-icon"]}
-          src="assets/magnifying-glass-solid.svg"
-          alt=""
-          onClick={() => search(document.getElementById("search_input").value)}
-        />
-      </div>
-      <div className={classes.slideOutMenuContainer}>
-        <SlideoutMenu pageWrapId="page-wrap" outerContainer="outer-container" />
+      <div className={classes.rightSide}>
+        <div className={classes.searchContainer}>
+          <div className={(classes.searchTextInput, getVisibilityClass())}>
+            <TextInput placeholder="Search" callback={search} />
+          </div>
+          <div className={classes.searchIcon}>
+            <img
+              height="25px"
+              className={classes["search-icon"]}
+              src="assets/magnifying-glass-solid.svg"
+              alt=""
+              onClick={() => {
+                setVisibility(!visibility);
+              }}
+            />
+          </div>
+        </div>
+
+        <div className={classes.slideOutMenuContainer}>
+          <SlideoutMenu
+            pageWrapId="page-wrap"
+            outerContainer="outer-container"
+          />
+        </div>
       </div>
     </div>
   );
