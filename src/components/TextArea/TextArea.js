c4b35c80a6ef90c15670b356010618cd424a0af0
Przemysław Gajowczyk <przemyslaw.gajowczyk23@gmail.com>
2022-02-08 20:46:32 +0100

    fix

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { forwardRef } from "react";
 import PropTypes from "prop-types";
 import classes from "./TextArea.module.scss";
@@ -47,53 +46,3 @@ TextArea.defaultProps = {
 };
 
 export default TextArea;
-=======
-import React, { forwardRef } from "react";
-import PropTypes from "prop-types";
-import classes from "./TextArea.module.scss";
-
-const TextArea = forwardRef(
-  ({ placeholder, callback, onChange, onBlur, name }, ref) => {
-    const handleKeyDown = (e) => {
-      if (e.key === "Enter") {
-        callback(document.getElementById("search_input").value);
-      }
-    };
-
-    return (
-      <div className={classes["textarea-container"]}>
-        <i className={classes.icon}>&gt;</i>
-        <textarea
-          name={name}
-          ref={ref}
-          onChange={onChange}
-          onBlur={onBlur}
-          className={classes["textarea-input"]}
-          type="text"
-          placeholder={placeholder}
-          id="search_input"
-          onKeyDown={(e) => handleKeyDown(e)}
-        />
-      </div>
-    );
-  }
-);
-
-TextArea.propTypes = {
-  placeholder: PropTypes.string,
-  callback: PropTypes.func,
-  onChange: PropTypes.func,
-  onBlur: PropTypes.func,
-  name: PropTypes.string
-};
-
-TextArea.defaultProps = {
-  placeholder: "_____",
-  callback: () => {},
-  onChange: () => {},
-  onBlur: () => {},
-  name: ""
-};
-
-export default TextArea;
->>>>>>> origin/dev
