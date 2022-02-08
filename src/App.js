33956604a5c3de4b8cf5fe0c85af67a39c8bd9a9
KrisGum <krisgumularz@gmail.com>
2022-02-08 22:41:04 +0100

    Add global routing and style fixes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import makeServer from "./server";
 
-import AddRecipe from "./pages/AddRecipe/AddRecipe";
 import Layout from "./Layout";
+import AddRecipe from "./pages/AddRecipe/AddRecipe";
+import AddedRecipe from "./pages/AddedRecipe/AddedRecipe";
 import BrowserPage from "./pages/BrowserPage/BrowserPage";
+import HomePage from "./pages/HomePage/HomePage";
+import GenerateMenuPage from "./pages/GenerateMenuPage/GenerateMenuPage";
+import GeneratedMenuPage from "./pages/GeneratedMenuPage/GeneratedMenuPage";
 
 const App = () => {
   makeServer();
@@ -12,8 +16,12 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<AddRecipe />} />
+        <Route index element={<HomePage />} />
+        <Route path="/addrecipe-page" element={<AddRecipe />} />
+        <Route path="/addedrecipe-page" element={<AddedRecipe />} />
         <Route path="/browser-page" element={<BrowserPage />} />
+        <Route path="/generatemenu-page" element={<GenerateMenuPage />} />
+        <Route path="/generetedmenu-page" element={<GeneratedMenuPage />} />
       </Route>
     </Routes>
   );
