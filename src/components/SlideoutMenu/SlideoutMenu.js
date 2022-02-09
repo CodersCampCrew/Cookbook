f783f7f4c65d4eb8c314084f6dffdb933d5ec26f
Marcin <marcinkukulka@gmail.com>
2022-02-09 11:37:54 +0100

    Fix style

--- src/components/SlideoutMenu/SlideoutMenu.js
+++ src/components/SlideoutMenu/SlideoutMenu.js
@@ -1,9 +1,8 @@
 import { slide as Menu } from "react-burger-menu";
 import Hamburger from "hamburger-react";
 import { useState } from "react";
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 import classes from "./SlideoutMenu.module.scss";
-
 
 const SlideoutMenu = () => {
   const [isOpen, setOpen] = useState(false);
@@ -15,13 +14,24 @@ const SlideoutMenu = () => {
       noOverlay
       customBurgerIcon={<Hamburger toggled={isOpen} toggle={setOpen} />}
       onClick={() => setOpen(!isOpen)}
-      width={200}
+      width={250}
+      disableCloseOnEsc
     >
-      <NavLink to="/addrecipe-page"><h3 className={classes.menuItem}>Breakfast</h3></NavLink>
-      <NavLink to="/addedrecipe-page"><h3 className={classes.menuItem}>Lunch</h3></NavLink>
-      <NavLink to="/browser-page"><h3 className={classes.menuItem}>Dinner</h3></NavLink>
-      <NavLink to="/generatemenu-page"><h3 className={classes.menuItem}>Others</h3></NavLink>
-      <NavLink to="/generetedmenu-page"><h3 className={classes.menuItem}>Others</h3></NavLink>
+      <NavLink className={classes.link} to="/home-page">
+        <h3 className={classes.menuItem}>Home</h3>
+      </NavLink>
+      <NavLink className={classes.link} to="/addrecipe-page">
+        <h3 className={classes.menuItem}>Add recipe</h3>
+      </NavLink>
+      <NavLink className={classes.link} to="/browser-page">
+        <h3 className={classes.menuItem}>Dinner</h3>
+      </NavLink>
+      <NavLink className={classes.link} to="/generatemenu-page">
+        <h3 className={classes.menuItem}>Others</h3>
+      </NavLink>
+      <NavLink className={classes.link} to="/generetedmenu-page">
+        <h3 className={classes.menuItem}>Others</h3>
+      </NavLink>
     </Menu>
   );
 };
