89a9ad48d383bd61f5af7b3fbb694d48e5740259
Nilphym <nilphym@gmail.com>
2022-02-10 13:20:30 +0100

    Add min to time label

--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -34,7 +34,7 @@ const RecipePage = () => {
 
       <div className={classes.recipeBagdes}>
         <Label className={classes.bagde} labelName={`kcal ${dish.kcal}`} />
-        <Label className={classes.bagde} labelName={`time ${dish.time}`} />
+        <Label className={classes.bagde} labelName={`time ${dish.time}min`} />
       </div>
       <div className={classes.recipeDescription}>
         <p>{dish.desc}</p>
